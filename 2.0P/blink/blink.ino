8a5c285df61f19e2ca668e646de85e5a4d15ce05
Nicole <nicolebirchh@gmail.com>
2026-08-02 12:46:56 +1000

    included the println statement for Read_GPIO function, added comments

--- 2.0P/blink/blink.ino
+++ 2.0P/blink/blink.ino
@@ -1,17 +1,31 @@
 #include "GPIO.h"
+#include <sam.h>
 
-GPIO_Pin LED_1 = { PORT_A, 7, GPIO_OUTPUT }; // will map to D4 
+GPIO_Pin LED_1 = { PORT_A, 17, GPIO_OUTPUT }; // will map to D13
 
+
+/*
+  A simple blink function that takes a duration argument in seconds and 
+  writes HIGH then LOW to the pin. Calls the blocking delay() for the set
+  duration and Write_GPIO from the GPIO.h driver to configure the output
+*/
 void blink(int duration){
   Write_GPIO(&LED_1, true);
+  Serial.println(Read_GPIO(&LED_1));
   delay(duration*1000);
+
   Write_GPIO(&LED_1, false);
+  Serial.println(Read_GPIO(&LED_1));
   delay(duration*1000);
 }
+
 void setup() {
+  Serial.begin(9600);
+  while(!Serial);
   Config_GPIO(&LED_1);
 }
 
 void loop() {
+  // Blink in-built LED for 1 second 
   blink(1);
 }
